bcec543ae1e3df47f95bc70a31d3619e4a5a1488
Johannes Mols <johannes.mols@googlemail.com>
2021-11-25 13:32:11 +0100

    Fixed bug

--- main.py
+++ main.py
@@ -16,20 +16,20 @@ def setup_mqtt_client(broker: str, port: int) -> Client:
 
 if __name__ == '__main__':
     mqtt_client = setup_mqtt_client(sys.argv[2], int(sys.argv[3]))
-    topic = sys.argv[4]
+    base_topic = sys.argv[4]
     delay = float(sys.argv[5])
 
     file = sys.argv[1]
     variant = xes_importer.Variants.ITERPARSE
     parameters = {variant.value.Parameters.TIMESTAMP_SORT: True}
-    log = xes_importer.apply(file,variant=variant, parameters=parameters)
+    log = xes_importer.apply(file, variant=variant, parameters=parameters)
     df = log_converter.apply(log, variant=log_converter.TO_DATA_FRAME)
 
     file_name = Path(file).stem
 
     for index, row in df.iterrows():
         row_d = row.to_dict()
-        topic = f'{topic}/{file_name}/{row_d["case:concept:name"]}/{row_d["concept:name"]}'
+        topic = f'{base_topic}/{file_name}/{row_d["case:concept:name"]}/{row_d["concept:name"]}'
         payload = dict()
         if 'time:timestamp' in row_d:
             timestamp = arrow.get(row_d['time:timestamp'])
